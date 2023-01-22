0d597604c8f888e5878e8f4e67a85784979d2884
Ruhong <seahrh@gmail.com>
2023-01-22 11:58:20 +0800

    add type embeddings to word embeddings

--- src/mylib/word2vec.py
+++ src/mylib/word2vec.py
@@ -14,13 +14,16 @@ class SkipGramDataset(Dataset):
     def __init__(
         self,
         center_words: List[int],
-        session_ids: Optional[List[int]] = None,
-        outside_words: Optional[List[int]] = None,
+        center_types: List[int],
+        outside_words: List[int],
+        outside_types: List[int],
     ):
-        self.encoding = {"center_words": center_words}
-        if outside_words is not None:
-            self.encoding["outside_words"] = outside_words
-        self.session_ids: List[int] = session_ids if session_ids is not None else []
+        self.encoding = {
+            "center_words": center_words,
+            "center_types": center_types,
+            "outside_words": outside_words,
+            "outside_types": outside_types,
+        }
 
     def __getitem__(self, idx):
         return {key: torch.tensor(val[idx]) for key, val in self.encoding.items()}
@@ -31,9 +34,6 @@ class SkipGramDataset(Dataset):
     def labels(self) -> List[int]:
         return list(self.encoding.get("outside_words", []))
 
-    def groups(self) -> List[int]:
-        return self.session_ids
-
 
 # noinspection PyAbstractClass
 class SkipGramWord2Vec(pl.LightningModule):
@@ -41,6 +41,7 @@ class SkipGramWord2Vec(pl.LightningModule):
         self,
         lr: float,
         vocab_size: int,
+        types_size: int,
         embedding_size: int,
         noise_dist: Optional[List[float]] = None,
         negative_samples: int = 10,
@@ -49,8 +50,10 @@ class SkipGramWord2Vec(pl.LightningModule):
         super().__init__()
         self.automatic_optimization = True
         self.lr = lr
-        self.embeddings = nn.Embedding(vocab_size, embedding_size)
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_size)
+        self.type_embeddings = nn.Embedding(types_size, embedding_size)
         self.vocab_size = vocab_size
+        self.types_size = types_size
         self.negative_samples = negative_samples
         self.noise_dist = (
             noise_dist if noise_dist is not None else torch.ones(self.vocab_size)
@@ -58,14 +61,23 @@ class SkipGramWord2Vec(pl.LightningModule):
         # Copied from transformers.models.bert.modeling_bert.BertPreTrainedModel._init_weights
         # initializer_range(`float`, *optional *, defaults to 0.02):
         # The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
-        self.embeddings.weight.data.normal_(mean=0.0, std=initializer_range)
+        self.word_embeddings.weight.data.normal_(mean=0.0, std=initializer_range)
+        self.type_embeddings.weight.data.normal_(mean=0.0, std=initializer_range)
+        # Initialize both embedding tables with uniform distribution
+        # self.embeddings.weight.data.uniform_(-1, 1)
 
-    def forward(self, center_words, outside_words):
+    def forward(self, center_words, center_types, outside_words, outside_types):
         log.debug(
             f"center_word.size={center_words.size()}, outside_word.size={outside_words.size()}"
         )  # bs
-        em_center = self.embeddings(center_words)  # bs, emb_dim
-        em_outside = self.embeddings(outside_words)  # bs, emb_dim
+        # bs, emb_dim
+        em_center = self.word_embeddings(center_words) + self.type_embeddings(
+            center_types
+        )
+        # bs, emb_dim
+        em_outside = self.word_embeddings(outside_words) + self.type_embeddings(
+            outside_types
+        )
         log.debug(
             f"em_center.size={em_center.size()}, em_outside.size={em_outside.size()}"
         )
@@ -91,7 +103,9 @@ class SkipGramWord2Vec(pl.LightningModule):
             ).to(self.device)
             log.debug(f"neg_input_ids.size={neg_input_ids}")
             # bs, neg_samples, emb_dim
-            em_neg = self.embeddings(neg_input_ids)
+            em_neg = self.word_embeddings(neg_input_ids) + self.type_embeddings(
+                torch.zeros(neg_input_ids.size())
+            )
             log.debug(f"em_neg.size={em_neg.size()}")
             # batch matrix multiply
             # (B, K, D) * (B, D, 1) = (B, K, 1)
